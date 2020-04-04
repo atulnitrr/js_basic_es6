d29a292617cbd92475c2a631b7c18a34e8687838
Atul Kumar <atulkumar458@gmail.com>
2020-04-04 15:53:11 +0530

    Every and some operator

--- EveryAndSome.js
+++ EveryAndSome.js
@@ -1,3 +1,30 @@
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some(function(request){
+  return Object.is(request.status, "pending");
+});
+
+console.log(inProgress);
+
+
+
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every(function(user){
+  return user.hasSubmitted;
+});
+
+console.log(hasSubmitted);
+
 var computers = [
   {name: "Apple", ram: 18},
   {name: "Mac", ram: 23},
