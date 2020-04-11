d63d4418ea25db0592d62db91ab4e2fb2642099e
Atul Kumar <atulkumar458@gmail.com>
2020-04-11 17:03:45 +0530

    Bind function

--- thiskey/arrowexp.js
+++ thiskey/arrowexp.js
@@ -1,1 +1,13 @@
 console.log("arrow example --> ");
+
+
+const person = {
+  firstName: "amit", 
+  printName: function() {
+    console.log(`${this.firstName}`);
+  }
+}
+
+person.printName();
+const dummuy = person.printName;
+dummuy.bind(person)();
