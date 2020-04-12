822a68ef8a0b2096085b3699e3f030692679eb1f
Atul Kumar <atulkumar458@gmail.com>
2020-04-12 13:50:27 +0530

    use strict mode

---
+++ advancedjs/basics/basic.js
@@ -0,0 +1,36 @@
+"use strict"
+console.log("basic js");
+
+
+
+/*
+
+if we dont use strict mode then it will print 12 , in case of strict mode it wil print 2
+var a = 2;
+eval("var a = 12");
+console.log(a);
+
+*/
+
+
+
+/*
+We can do this is normal mode but cant do in strict mode
+var foo = 1; 
+delete foo;
+
+*/
+
+
+/*
+var let = 90;
+console.log(let);
+*/
+
+/*
+const res = 10;
+console.log(res);
+
+*/
+
+
