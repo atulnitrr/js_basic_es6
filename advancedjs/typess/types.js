d1395247365f4d2ef86fd203072ef04fee26337d
Atul Kumar <atulkumar458@gmail.com>
2020-04-12 16:22:36 +0530

    Type of operator

---
+++ advancedjs/typess/types.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("types js");
+
+
+
+/*
+console.log(typeof(1));
+console.log(typeof("44"));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof({}));
+*/
+
