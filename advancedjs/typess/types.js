613d980e71baa001092ce7dccf4349797056c351
Atul Kumar <atulkumar458@gmail.com>
2020-04-12 16:50:37 +0530

    NaN

--- advancedjs/typess/types.js
+++ advancedjs/typess/types.js
@@ -3,6 +3,30 @@ console.log("types js");
 
 
 
+var a = NaN;
+console.log(a !== a);
+a = 34
+console.log(a !== a);
+a = "sss"
+console.log(a !== a);
+
+
+/*
+console.log(isNaN(NaN));
+
+console.log(NaN === NaN);
+
+console.log(("abc" / 56));
+
+
+console.log(typeof(NaN));
+*/
+
+/*
+var a; 
+console.log(a);
+*/
+
 /*
 console.log(typeof(1));
 console.log(typeof("44"));
