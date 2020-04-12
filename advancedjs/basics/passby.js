0d0d312ea0ac90a32208b8180927e2339736f01e
Atul Kumar <atulkumar458@gmail.com>
2020-04-12 15:49:08 +0530

    Template tags

--- advancedjs/basics/passby.js
+++ advancedjs/basics/passby.js
@@ -2,6 +2,74 @@ console.log("pass by js");
 "use strict"
 
 
+
+
+
+
+function h1(strings, ...values) {
+
+  let body = "";
+  for (let i = 0; i < strings.length; i++) {
+    body += strings[i] + (values[i] || "");
+  }
+  return `<h1>${body}</h1>`
+}
+
+const name = "atul";
+const place = "hyd"
+console.log(h1`My name is ${name} , place is ${place}`);
+;
+
+
+/*
+
+function h1(string) {
+  return "<h1>" + string[0] + "</h1>";
+}
+
+console.log(h1`atul`);
+
+*/
+
+
+/*
+const bold =  false;
+
+const res = `Heelo ${bold ? "<b>atul</b>" : "atul"}`
+console.log(res);
+
+
+const dummy = `heelo ${2 + 3}`;
+console.log(dummy);
+*/
+
+
+/*
+
+const myName =  `hello's 
+my nameis 
+"atul"`;
+console.log(myName);
+*/
+ 
+
+/*
+const myName = "hello \n my name \n is \n atul";
+console.log(myName);
+
+*/
+
+/*
+const myName = "my name is atul";
+console.log(myName);
+*/
+
+/*
+console.log(`hello 
+data`);
+
+*/
+
 /*
 const array1 = [1, 2, 3];
 const array2 = [3, 4, 4, ...array1];
