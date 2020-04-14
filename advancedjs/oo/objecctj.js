cae56b7c9e834352304b20c42a88a82197e2f78e
Atul Kumar <atulkumar458@gmail.com>
2020-04-14 08:34:48 +0530

    Passing properties

--- advancedjs/oo/objecctj.js
+++ advancedjs/oo/objecctj.js
@@ -2,6 +2,24 @@ console.log("Object orientation-->");
 
 
 
+const animal = {
+  kind: "human"
+}
+
+// const dummy = Object.create(animal);
+// const dummy = Object.create(animal, {food: {value: [1, 2,4]}});
+
+const dummy = Object.create(animal, {food: {value:  "data"}, shelter: {value: "home"}});
+
+console.log(dummy);
+console.log(animal);
+
+
+
+
+
+
+
 /*
 const animal = {
   kind: "human"
