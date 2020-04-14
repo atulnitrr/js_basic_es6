a7c9d851dcd13724651a0134aaa95879dcdabe21
Atul Kumar <atulkumar458@gmail.com>
2020-04-14 08:27:51 +0530

    protot

---
+++ advancedjs/oo/objecctj.js
@@ -0,0 +1,26 @@
+console.log("Object orientation-->");
+
+
+
+/*
+const animal = {
+  kind: "human"
+}
+
+console.log(animal);
+// console.log(animal.__proto__);
+
+const atul = {
+}
+
+atul.__proto__ = animal;
+
+console.log(atul);
+console.log(atul.kind);
+console.log(animal.isPrototypeOf(atul));
+animal.kind = "toto";
+console.log(atul.kind);
+
+*/
+
+
