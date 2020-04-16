8ae4337826f23ccde1da8159c0cb913e91de659c
Atul Kumar <atulkumar458@gmail.com>
2020-04-16 11:51:25 +0530

    Prevent default

--- advancedjs/networking/event/eventjs.js
+++ advancedjs/networking/event/eventjs.js
@@ -1,9 +1,45 @@
 console.log("Event bubbling ");
 
 
+
+const item = document.getElementById("pass");
+
+item.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(event);
+  
+})
+
+console.log(item);
+
+
+
+/*
 const items = document.getElementsByClassName("item");
 
+*/
 
+/*
+for (let index = 0; index < items.length; index++) {
+  
+  (function(y) {
+    const element = items[y];
+    element.addEventListener("click", event => {
+        event.preventDefault();
+      // if (y === 2 ) {
+      //   event.stopPropagation();
+      // }
+      // alert(y);
+      console.log(element, y, event);
+      
+    }, true)
+  })(index);
+  
+}
+*/
+
+
+/*
 for (let index = 0; index < items.length; index++) {
   
   (function(i) {
@@ -15,4 +51,4 @@ for (let index = 0; index < items.length; index++) {
 }
 
 console.log(items);
-
+*/
